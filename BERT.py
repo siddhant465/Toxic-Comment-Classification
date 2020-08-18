df5ae3a5bd5c2247f351f4b12277cd25ed194c72
Siddhant Deshmukh <siddhantdeshmukh@localhost.localdomain>
2020-08-18 15:49:21 -0700

    Added Report

--- BERT.py
+++ BERT.py
@@ -1,39 +1,28 @@
 #!/usr/bin/env python
-
 import pandas as pd
 from simpletransformers.classification import MultiLabelClassificationModel
 import sys, os, re, csv, codecs, numpy as np, pandas as pd
-
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.layers import Dense, Input, LSTM, Embedding, Dropout, Activation
 from keras.layers import Bidirectional, GlobalMaxPool1D
 from keras.models import Model
 from keras import initializers, regularizers, constraints, optimizers, layers
-
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
-
 from sklearn.model_selection import train_test_split
 
-
 # Used for Loading Files later
-
 training_file = f'train.csv'
 testing_file = f'test.csv'
 
-
 # Loading Data
-
 train = pd.read_csv(training_file)
 test = pd.read_csv(testing_file)
 
-
 # Preprocessing the Data
-
 # Remove Punctuation
-
 def remove_punctuation(text):
     '''a function for removing punctuation'''
     import string
@@ -43,9 +32,7 @@ def remove_punctuation(text):
 train['comment_text'] = train['comment_text'].apply(remove_punctuation)
 test['comment_text'] = test['comment_text'].apply(remove_punctuation)
 
-
 # Remove Stop Words
-
 sw=stopwords.words('english')
 
 def removesw(text):
@@ -58,9 +45,7 @@ def removesw(text):
 train['comment_text'] = train['comment_text'].apply(removesw)
 test['comment_text'] = test['comment_text'].apply(removesw)
 
-
 # Applying Stemming
-
 stemmer = SnowballStemmer("english")
 
 def stemming(text):    
@@ -70,9 +55,7 @@ def stemming(text):
 train['comment_text'] = train['comment_text'].apply(stemming)
 test['comment_text'] = test['comment_text'].apply(stemming)
 
-
 # Create DistilBERT model for Multi-label Classification
-
 model = MultiLabelClassificationModel(
     "distilbert",
     "distilbert-base-uncased",
@@ -82,18 +65,15 @@ model = MultiLabelClassificationModel(
 )
 
 # Create compatible dataframe
-
 train['labels'] = list(zip(train.toxic.tolist(), train.severe_toxic.tolist(), train.obscene.tolist(), train.threat.tolist(),  train.insult.tolist(), train.identity_hate.tolist()))
 train['text'] = train['comment_text'].apply(lambda x: x.replace('\n', ' '))
 
 train.head()
 
-
 # Split dataset for validation
 train_df, eval_df = train_test_split(train, test_size = 0.2)
 
 model.train_model(train_df, output_dir="outputs/")
-
 
 # Evaluate the model
 result, model_outputs, wrong_predictions = model.eval_model(eval_df)
@@ -101,7 +81,6 @@ result, model_outputs, wrong_predictions = model.eval_model(eval_df)
 # Create Submission for Testing on Kaggle
 
 test_df = pd.read_csv(testing_file)
-
 to_predict = test_df.comment_text.apply(lambda x: x.replace('\n', ' ')).tolist()
 preds, outputs = model.predict(to_predict)
 
@@ -118,8 +97,3 @@ submission_df.to_csv('outputs/submission.csv', index=False)
 # 
 # We used the SimpleTransformers library to use pre-trained models in an easy way.
 # https://github.com/ThilinaRajapakse/simpletransformers
-
-
-
-
-
